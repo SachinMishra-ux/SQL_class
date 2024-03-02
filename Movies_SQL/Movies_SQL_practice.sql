48a4964f4c23417bffa188a2c010f55240cb2bc6
Sachin Mishra <sachin19566@gmail.com>
2024-03-02 11:10:57 +0530

    files organized

--- Movies_SQL/Movies_SQL_practice.sql
+++ Movies_SQL/Movies_SQL_practice.sql
@@ -30,6 +30,32 @@ FROM movies m
 INNER JOIN revenue r ON m.movie_id = r.movie_id
 GROUP BY m.title, m.director;
 
+-- Retrieve the title, revenue, and profit (revenue - budget) for movies released after 2000, sorted by profit in descending order.
+
+SELECT m.title, r.revenue, (r.revenue - r.budget) AS profit
+FROM movies m
+INNER JOIN revenue r ON m.movie_id = r.movie_id
+WHERE r.release_year > 2000
+ORDER BY profit DESC;
+
+-- Find the average return on investment (ROI) for movies released in the 1990s. ROI is calculated as (revenue - budget) / budget * 100.
+
+SELECT AVG(((r.revenue - r.budget) / r.budget) * 100) AS avg_roi
+FROM movies m
+INNER JOIN revenue r ON m.movie_id = r.movie_id
+WHERE r.release_year BETWEEN 1990 AND 1999;
+
+-- List the directors along with the total revenue and average budget of the movies they directed, for directors who have directed at least 2 movies.
+
+SELECT m.director, SUM(r.revenue) AS total_revenue, AVG(r.budget) AS avg_budget
+FROM movies m
+INNER JOIN revenue r ON m.movie_id = r.movie_id
+GROUP BY m.director
+HAVING COUNT(m.movie_id) >= 2;
+
+
+
+
 
 
 
